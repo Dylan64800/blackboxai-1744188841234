5fd43364a1d9d0f355cf11be12b1778f80f6d4dc
Dylan64800 <dylan.navelet@gmail.com>
2025-04-09 10:54:07 +0200

    Add tailwind.config.js 1744188848146

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#5a7d5a',
+        secondary: '#f9f7f3',
+        accent: '#4a6d4a'
+      },
+      fontFamily: {
+        'playfair': ['"Playfair Display"', 'serif'],
+        'montserrat': ['Montserrat', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+}
